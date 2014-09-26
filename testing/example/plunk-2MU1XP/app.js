d32217a39d85dfcd182f8eb19d0be2f828a75269
Michal Lipski <kiwaczki@gmail.com>
2014-09-26 10:32:52 +0200

    fixes

--- testing/example/plunk-2MU1XP/app.js
+++ testing/example/plunk-2MU1XP/app.js
@@ -2,10 +2,17 @@ var app = angular.module('plunker', []);
 app.directive('hello', function () {
     return {
         restrict: 'E',
-        template: '<div>Hello {{name}}</div>'
+        template: '<button ng-click="sayHello()">Hello {{name}}</button>',
+	compile:function(element){
+		//element.attr('ng-click','sayHello()')
+	}
     };
 });
 
 app.controller('MainCtrl', function ($scope) {
     $scope.name = 'World';
+    $scope.sayHello = function(){
+	console.log('dsdsd');
+	alert('hello');	
+    }
 });
